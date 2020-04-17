80874784a5a8a31370c10c2199bc691e6d1d2a61
Julio Jimenez <juli.jimenez@alumnos.duoc.cl>
2020-04-17 10:15:59 -0400

    Servicio ApiRest Login No funcionando- sin navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { RestapiService } from './restapi.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RestapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestapiService } from '../restapi.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username:string;
+  password:string;
+  message:any;
+
+  constructor(private service:RestapiService) { }
 
   ngOnInit(): void {
+  }
+
+  doLogin(){
+    let resp= this.service.signin(this.username, this.password);
+    resp.subscribe(data=>{
+      console.log(data)
+    })
   }
 
 }
