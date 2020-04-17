a8a427f3abcc38623a976b43192c2efd5bbe64c9
Marco <marcoastorga.g@gmail.com>
2020-04-17 12:08:09 -0400

    update estructura test login estatico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './web/login/login.component';
+import { RegisterComponent } from './web/register/register.component';
 
 
 const routes: Routes = [
