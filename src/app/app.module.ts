c0e9a45a0c2447cbe624a26ed4429068fc993c15
Marco <marcoastorga.g@gmail.com>
2020-04-24 10:58:34 -0400

    update navbar y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './web/login/login.component';
 import { RegisterComponent } from './web/register/register.component';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './widget/navbar/navbar.component';
+import { FooterComponent } from './widget/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
