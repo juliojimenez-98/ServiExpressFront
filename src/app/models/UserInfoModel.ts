30ced57764a843b17accf85c3a3266a476ae624a
marco <marcoastorga.g@gmail.com>
2020-04-30 13:56:26 -0400

    clean

--- src/app/models/UserInfoModel.ts
+++
@@ -1,9 +0,0 @@
-export class UserInfoModel {
-    idCliente: number;
-    idUsuario: number;
-    rut: string;
-    nombre: string;
-    apellido: string;
-    telefono: string;
-    fechaN: string;
-}
--- src/app/models/UserModel.ts
+++
@@ -1,8 +0,0 @@
-export class UserModel{
-    name : string;
-	username : string;
-	email : string;
-	password : string;
-  password2 : string;
-    role : string;
-}
