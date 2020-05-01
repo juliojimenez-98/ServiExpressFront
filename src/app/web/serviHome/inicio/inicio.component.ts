c7a25eacfcba2f8d127b2ee3d4746e16c3de5b61
marco <marcoastorga.g@gmail.com>
2020-04-30 21:52:15 -0400

    fix js and css

--- src/app/web/serviHome/inicio/inicio.component.ts
+++ src/app/web/serviHome/inicio/inicio.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarService } from 'src/app/service/navbar.service';
-
+// declare var drawGauge: any;
+// import '../../../../assets/js/sb-admin-2.min.js';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+
+
 
 
 
@@ -17,6 +20,25 @@ export class InicioComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    this.loadScript('../assets/js/sb-admin-2.min.js');
+    this.loadScript('../assets/vendor/jquery/jquery.min.js');
+    // this.loadScript('../assets/vendor/bootstrap/js/bootstrap.bundle.min.js');
+    this.loadScript('../assets/vendor/jquery-easing/jquery.easing.min.js');
+    this.loadScript('../assets/vendor/chart.js/Chart.min.js');
+    this.loadScript('../aassets/js/demo/chart-area-demo.js');
+    this.loadScript('../assets/js/demo/chart-pie-demo.js');
+
+  }
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
   }
 
 }
