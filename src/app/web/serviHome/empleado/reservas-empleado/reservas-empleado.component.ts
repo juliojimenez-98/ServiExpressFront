bb318e3206310e4849e3cd4f4e3558be9f1e0cde
Julio Jimenez <j.jimenezv098@gmail.com>
2020-06-07 18:10:01 -0400

    obtener id reserva desde la tabla

--- src/app/web/serviHome/empleado/reservas-empleado/reservas-empleado.component.ts
+++ src/app/web/serviHome/empleado/reservas-empleado/reservas-empleado.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { ReservaResponse } from "src/app/models/ReservaResponse";
 import { NegocioService } from 'src/app/service/negocio.service';
+import { Reserva } from 'src/app/models/reserva';
 
 
 @Component({
@@ -9,9 +10,12 @@ import { NegocioService } from 'src/app/service/negocio.service';
   templateUrl: './reservas-empleado.component.html'
 })
 export class ReservasEmpleadoComponent implements OnInit {
-
+  estados;
   reservas: ReservaResponse[];
-  constructor(private service:NegocioService) { }
+  private reserva:Reserva = new Reserva;
+  constructor(private service:NegocioService) {
+    // this.estados = ['En Taller', 'Revicion', 'Trabajando', 'Limpieza', 'Terminando', 'Servicio Completo'];
+  }
 
   ngOnInit(): void {
     this.getReservas();
@@ -25,25 +29,39 @@ export class ReservasEmpleadoComponent implements OnInit {
 
 
 
-  async estadoReserva():Promise<void>{
+  async estadoReserva(reserva):Promise<void>{
+    console.log(reserva)
+  //   reserva.forEach(resource => {
+  //     console.log(resource.idreserva);
+
+  // });
     const { value: estado } = await Swal.fire({
       title: 'Selecciona el estado',
       input: 'select',
       position:"center",
       inputOptions: {
-        EnProceso: 'En Proceso',
-        EnTaller: 'En Taller',
-        CasiListo: 'Casi Listo',
-        Listo: 'Listo'
+
+        1: 'Revision',
+        2: 'Trabajando',
+        3: 'Limpieza',
+        4: 'Terminando',
+        5: 'Servicio Completo'
       },
       inputPlaceholder: 'Selecciona el estado de la reserva',
       showCancelButton: true,
 
     })
 
+
+
     if (estado) {
+
       Swal.fire('Estado de reserva',`El estado cambió a: ${estado}`,'success')
+      console.log(reserva)
+      console.log(estado)
     }
+
   }
+
 
 }
